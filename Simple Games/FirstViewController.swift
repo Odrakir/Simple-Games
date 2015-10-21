8b2037d9c34aa5c5c6e5cff58f0458a55e792d85
Ricardo Sánchez Sotres <odrakir@gmail.com>
2015-10-21 19:25:24 +0200

    XCode setup

---
+++ Simple Games/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Simple Games
+//
+//  Created by Ricardo Sánchez Sotres on 21/10/15.
+//  Copyright © 2015 Ricardo Sánchez Sotres. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
