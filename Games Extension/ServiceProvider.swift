810ab2b6e2c552a727c81aebc2c5543f3ff31d7f
Ricardo Sánchez Sotres <odrakir@gmail.com>
2015-10-24 10:23:13 +0200

    Top shelf extension

---
+++ Games Extension/ServiceProvider.swift
@@ -0,0 +1,56 @@
+//
+//  ServiceProvider.swift
+//  Games Extension
+//
+//  Created by Ricardo Sánchez Sotres on 23/10/15.
+//  Copyright © 2015 Ricardo Sánchez Sotres. All rights reserved.
+//
+
+import Foundation
+import TVServices
+
+class ServiceProvider: NSObject, TVTopShelfProvider {
+    
+    override init() {
+        super.init()
+    }
+    
+    // MARK: - TVTopShelfProvider protocol
+    
+    var topShelfStyle: TVTopShelfContentStyle {
+        // Return desired Top Shelf style.
+        return .Sectioned
+    }
+    
+    var topShelfItems: [TVContentItem] {
+        
+        var contentItems = [TVContentItem]();
+        
+        var item = TVContentItem(contentIdentifier: TVContentIdentifier(identifier: "theWall", container: nil)!)
+        item!.imageURL = NSBundle.mainBundle().URLForResource("theWall_cover.png", withExtension: nil)
+        item!.imageShape = .Square
+        
+        item!.displayURL = NSURL(string: "launchgame://theWall")
+        item!.title = "The Wall"
+        contentItems.append(item!)
+        
+        item = TVContentItem(contentIdentifier: TVContentIdentifier(identifier: "columns", container: nil)!)
+        item!.imageURL = NSBundle.mainBundle().URLForResource("columns_cover.png", withExtension: nil)
+        item!.imageShape = .Square
+        
+        item!.displayURL = NSURL(string: "launchgame://columns")
+        item!.title = "Columns"
+        contentItems.append(item!)
+        
+        
+        // SET THE SECTION DETAILS
+        let sectionItem = TVContentItem(contentIdentifier: TVContentIdentifier(identifier: "contentIdentifier", container: nil)!)
+        sectionItem!.title = "Simple Games"
+        sectionItem!.topShelfItems = contentItems
+        
+        
+        return [sectionItem!]
+    }
+
+}
+
